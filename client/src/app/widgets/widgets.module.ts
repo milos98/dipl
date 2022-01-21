bcd9d8cbfe63be7777c84d8516e5f14683c55bbd
milosFHC <milos.jankovic@florencehc.com>
2022-01-21 22:45:04 +0100

    added employee-info widget and some scss refactoring

--- client/src/app/widgets/widgets.module.ts
+++ client/src/app/widgets/widgets.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeadInfoComponent } from './lead-info/lead-info.component';
+import { EmployeeInfoComponent } from './employee-info/employee-info.component';
 
 
 
 @NgModule({
   declarations: [
-    LeadInfoComponent
+    LeadInfoComponent,
+    EmployeeInfoComponent
   ],
   imports: [
     CommonModule
