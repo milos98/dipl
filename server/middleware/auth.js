97197371d527c533c32868b0c8640c63daaf76a7
milosFHC <milos.jankovic@florencehc.com>
2022-03-10 02:14:33 +0100

    middleware update to check if user is suspended

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,17 +1,26 @@
 const jwt = require("jsonwebtoken");
 
 const config = require('./../config');
+const Dals = require('../dals')
 
-const verifyToken = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
   const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
+    req.body.token || req.query.token || req.headers["x-access-token"] || req.headers.authorization.split(' ')[1];
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
   try {
     const decoded = jwt.verify(token, config.auth.tokenKey);
-    req.user = decoded;
+    req.auth = decoded;
+    console.log(decoded);
+
+    const user = await Dals.users.findById(decoded.user_id);
+
+    if(!user.isSuspended) {
+      return res.status(403).send("User suspended");
+    }
+
   } catch (err) {
     return res.status(401).send("Invalid Token");
   }
