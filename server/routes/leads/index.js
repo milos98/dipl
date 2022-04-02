96c3aff08863f9e73f45947e6eb4a664e792e8d5
milosFHC <milos.jankovic@florencehc.com>
2022-04-03 01:56:11 +0200

    routes link update

--- server/routes/leads/index.js
+++ server/routes/leads/index.js
@@ -3,7 +3,7 @@ const LeadsRouter = require('express').Router();
 const Controllers = require('./controllers');
 
 LeadsRouter.get('/', Controllers.getAll);
-LeadsRouter.get('/users/:id', Controllers.getAllUsersLeads);
+LeadsRouter.get('/me', Controllers.getAllUsersLeads);
 LeadsRouter.post('/', Controllers.create);
 LeadsRouter.patch('/:id', Controllers.update);
 
